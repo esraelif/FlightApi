5effa65a90ebf6bed64a840412bce9b424ef5375
esraelif <elifesratunca@gmail.com>
2024-08-17 08:22:49 +0200

    "Updated reservation controller to include passenger processing and added permission checks"

--- src/controllers/reservation.js
+++ src/controllers/reservation.js
@@ -5,6 +5,8 @@ const { CustomError } = require("../errors/customError");
 ------------------------------------------------------- */
 
 const Reservation = require("../models/reservation");
+const Passenger = require("../models/passenger");
+const processPassengers = require("../helpers/processPassengers");
 
 module.exports = {
   list: async (req, res) => {
@@ -26,11 +28,12 @@ module.exports = {
       //* isAdmin ve isStaff değerlerinin ikisinin de false olması lazım
       customFilter = { createdId: req.user._id };
     }
-    const data = await res.getModelList(Reservation, customFilter);
+
+    const data = await res.getModelList(Reservation, customFilter, ["passengers", "createdId"]);
 
     res.status(200).send({
       error: false,
-      details: await res.getModelListDetails(Reservation),
+      details: await res.getModelListDetails(Reservation, customFilter),
       data,
     });
   },
@@ -46,6 +49,40 @@ module.exports = {
             }
         */
     req.body.createdId = req.user._id;
+    // [
+    //   "6694e8b89904ed18fd8ccd3f",
+    //   "6694f099503211178a406aac",
+    //   {
+    //     firstName: "Denemes",
+    //     lastName: "1denme",
+    //     email: "denemes1@deneme.com",
+    //   },
+    // ];
+    /* Check ID or OBJECT for passengers */
+    // let passengerInfos = req.body?.passengers || [],
+    //   passengerIds = [],
+    //   passenger = {};
+
+    // for (let passengerInfo of passengerInfos) {
+    //   if (typeof passengerInfo == "object") {
+    //     passenger = await Passenger.findOne({ email: passengerInfo.email }); //* reelde email değil unique olan tckn/passportId
+    //     if (!passenger) {
+    //       // passengerInfo = { ...passengerInfo, createdId: req.user._id };
+    //       Object.assign(passengerInfo, { createdId: req.user._id }); //* passenger create ederken benden createdId değeri bekliyor.
+
+    //       passenger = await Passenger.create(passengerInfo);
+    //     }
+    //   } else {
+    //     //* string olarak id bilgisi gelirse
+    //     passenger = await Passenger.findOne({ _id: passengerInfo }); //* id check
+    //   }
+
+    //   //* passenger bilgisi varsa passengera ait id bilgisi arraye ekle
+    //   if (passenger) passengerIds.push(passenger._id);
+    // }
+    const passengerIds = await processPassengers(Passenger, req.body?.passengers || [], req.user._id)
+    //* bodyye passengersların idlerini ata
+    req.body.passengers = passengerIds;
 
     const data = await Reservation.create(req.body);
 
@@ -59,13 +96,18 @@ module.exports = {
             #swagger.tags = ["Reservations"]
             #swagger.summary = "Get Single Reservation"
         */
-
-    if (!req.user.isAdmin && !req.user.isStaff) {
-      const checkData = await Reservation.findOne({ _id: req.params.id })
-      if (checkData.createdId != req.user._id) {
-        throw new CustomError("No Permisson! You must be admin or staff or own", 403)
-      }
-    }
+    //* isAdminOrStaffOrOwn
+    // if(!req.user.isAdmin && !req.user.isStaff) {
+    //   const checkData = await Reservation.findOne({ _id: req.params.id });
+    //   // console.log(typeof checkData.createdId); // object
+    //   // console.log(typeof req.user._id); // object
+    //   if (checkData.createdId?.toString() != req.user._id.toString()) {
+    //     throw new CustomError(
+    //       "NoPermission! You must be admin or staff or own!",
+    //       403
+    //     );
+    //   }
+    // }
     const data = await Reservation.findOne({ _id: req.params.id }).populate(
       "createdId"
     );
@@ -87,6 +129,16 @@ module.exports = {
             }
         */
     req.body.createdId = req.user._id;
+    //* isAdminOrStaffOrOwn
+    // if (!req.user.isAdmin && !req.user.isStaff) {
+    //   const checkData = await Reservation.findOne({ _id: req.params.id });
+    //   if (checkData.createdId?.toString() != req.user._id.toString()) {
+    //     throw new CustomError(
+    //       "NoPermission! You must be admin or staff or own!",
+    //       403
+    //     );
+    //   }
+    // }
     const data = await Reservation.updateOne({ _id: req.params.id }, req.body, {
       runValidators: true,
     });
